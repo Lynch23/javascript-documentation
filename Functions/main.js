f9dc8677ad02f5c471319f782e0fa9f60b38b890
SNEHA AGARWAL <sneha3agarwal@gmail.com>
2022-10-02 22:32:14 +0530

    Created JavaScript File for Functions

---
+++ Functions/main.js
@@ -0,0 +1,22 @@
+// Why do we need a function?
+ 
+//Suppose you have to add two numbers let say a and b.
+// Tehn you can simply add them by taking its value from the user and adding it by using a+b 
+// But what if you have to add 50 sets of such numbers?
+// You will repeat the above steps 50 times, right?
+// That pretty much doable but what if you are given 1000 such sets of numbers?
+// You won't be repeating it for 1000 times right?
+// That's where function comes in picture
+// They will produce reusuable codes.
+// You just have to define it once and then you can reuse it wherever you want.
+
+
+// Basic Syntax of a function in JavaScript
+
+function name(parameter1, parameter2, parameter3,...){
+//code that needs to be executed will go here
+}
+
+// There is a difference between parameter and arguement.
+// Parameter are variables that are used to take a particular value
+// Argument is the value that yoou give to the function so that some operations can be done on them.
